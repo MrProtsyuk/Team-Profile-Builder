827e82d188fc0633a9308f2153fdb92113a0951c
Your Name <markprotsyuk@Marks-MacBook-Pro-2.local>
2022-03-04 18:16:18 -0800

    created prompts for questions, not all. Created test and scripts but tests are all failing.

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,10 +1,36 @@
 const Employee = require('../lib/Employee.js');
 
 test('creates an employee object', () => {
+    const fakeName = "Mark";
+    const fakeId = '1';
+    const fakeEmail = 'Mark@me.com'
     const employee = new Employee('Mark');
 
-    expect(employee.name).toBe('Mark');
-    expect(employee.id).toBe('1');
-    expect(employee.email).toBe('Mark');
-    expect(employee.role).toBe('Employee');
+    expect(employee.name).toBe(fakeName);
+    expect(employee.id).toBe(fakeId);
+    expect(employee.email).toBe(fakeEmail);
+})
+
+test("gets employees name", () => {
+    const test = "Mark";
+    const employee = new Employee(test)
+    expect(employee.getName()).toBe(test);
+});
+
+test('gets employee id', () => {
+    const test = "1";
+    const employee = new Employee(test)
+    expect(employee.getId()).toBe(test);
+})
+
+test('gets employee email', () => {
+    const test = "Mark@me.com"
+    const employee = new Employee(test)
+    expect(employee.getEmail()).toBe(test)
+})
+
+test('gets employee role', () => {
+    const test = "Employee"
+    const employee = new Employee(test)
+    expect(employee.getEmail()).toBe(test)
 })
