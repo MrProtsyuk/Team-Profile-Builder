7823ffdc93094e51852162f2bb7130723b4dd5a8
Your Name <markprotsyuk@Marks-MacBook-Pro-2.local>
2022-03-06 21:12:07 -0800

    engineer tests all pass

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,11 +1,19 @@
 const Engineer = require('../lib/Engineer.js');
 
-test('creates an employee object', () => {
-    const engineer = new Engineer('Mark');
+test('can set github object', () => {
+    const test = "github";
+    const engineer = new Engineer("Mark", 1, "mark@mark", test);
+    expect(engineer.gitHub).toBe(test);
+})
 
-    expect(engineer.name).toBe('Mark');
-    expect(engineer.id).toBe('1');
-    expect(engineer.email).toBe('Mark');
-    expect(engineer.role).toBe('engineer');
-    expect(engineer.info).toBe('GitHub')
+test('get github username', () => {
+    const test = "github";
+    const enginner = new Engineer("mark", 1, "mark@mark", test);
+    expect(enginner.getGithub()).toBe(test);
+})
+
+test('gets enginner role', () => {
+    const test = "Engineer";
+    const engineer = new Engineer("mark", 1, "mark@mark", "github");
+    expect(engineer.getRole()).toBe(test);
 })
